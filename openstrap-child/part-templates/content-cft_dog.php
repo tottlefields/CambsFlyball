c73744ff5a3cb78f2f63d88b47e3411191fad8ab
Ellen Schofield <ellen.schofield@aht.org.uk>
2020-09-16 13:42:41 +0100

    spacer for no UKFL number

--- openstrap-child/part-templates/content-cft_dog.php
+++ openstrap-child/part-templates/content-cft_dog.php
@@ -9,6 +9,6 @@
 		<?php $ukfl_no = get_post_meta( get_the_ID() , 'ukfl_number', true ); ?>
 		<?php if ( isset($ukfl_no) && $ukfl_no != '' ) : ?>
 			<small>(<a href="https://www.ukflyball.org.uk/dogs/<?php echo $ukfl_no; ?>" title="<?php echo esc_attr( sprintf( __( 'UKFL Page for %s', 'openstrap' ), the_title_attribute( 'echo=0' ) ) ); ?>" target="_blank"><?php echo $ukfl_no;?></a>)</small>
-		<?php endif; ?>
+		<?php else : ?><small>&nbsp;</small><?php endif; ?>
 	</h4>
 </article>
