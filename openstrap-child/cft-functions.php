ad88f1750b469297e5a0510f039254ba4332ee66
Ellen Schofield <ellen.schofield@aht.org.uk>
2020-09-16 13:55:41 +0100

    corrected user link on accounts page

--- openstrap-child/cft-functions.php
+++ openstrap-child/cft-functions.php
@@ -93,7 +93,7 @@ function getRecentAccounts(){
 	global $wpdb;	
 	
 	$transactions = $wpdb->get_results("select date_in, case when method='INVOICE' then concat_ws(':', method, category, event_desc) else concat_ws(':', method, user_id) end as groupCol,
-	method, category, case when method='INVOICE' then event_desc else group_concat(distinct u.display_name) end as description, sum(amount) as total, group_concat(distinct user_id) as users, 
+	method, category, case when method='INVOICE' then event_desc else group_concat(distinct u.display_name) end as description, sum(amount) as total, group_concat(distinct u.user_login) as users, 
 	count(distinct user_id) as count
 	from cft_members_money m left outer join wp_users u on m.user_id=u.id
 	WHERE date_in > DATE_SUB(CURDATE(), INTERVAL 2 MONTH) and date_in <= CURDATE() group by 1,2 order by 1 desc,2");
