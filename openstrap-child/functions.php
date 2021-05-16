1fdc012df17851356eb64a2ee03f0729b8e382b6
Ellen Schofield <ellen@pawprintsdesign.co.uk>
2021-05-16 15:15:30 +0100

    trying to sort bootstrap out

--- openstrap-child/functions.php
+++ openstrap-child/functions.php
@@ -15,7 +15,7 @@ function enqueue_parent_styles() {
 
 	wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' , array('bootstrap'));
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri().'/style.css', array( 'parent-style' ), wp_get_theme()->get('Version') );
-	wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri().'/assets/css/bootstrap.css', array(), '3.0.0' );
+	wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri().'/assets/css/bootstrap.css', array(), '3.0.1' );
 	wp_enqueue_style( 'bootstrap-custom', get_template_directory_uri().'/css/custom.css', array('bootstrap'), '3.0.0' );
 
 }
