13270f99afe1cb51bf3a2ba0d566271e93e500e2
Ellen Schofield <ellen@pawprintsdesign.co.uk>
2022-06-21 14:50:45 +0100

    Update cft-widget.php

--- cambsflyball-plugin/widgets/cft-widget.php
+++ cambsflyball-plugin/widgets/cft-widget.php
@@ -40,7 +40,7 @@ class cft_widget extends WP_Widget {
             //<tr><td class="has-text-align-center" data-align="center">25th</td><td><a href="/teams/cambridgeshire-catapults/">Catapults</a></td><td class="has-text-align-center" data-align="center">16.92</td></tr><tr><td class="has-text-align-center" data-align="center">46th</td><td><a href="/teams/cambridgeshire-cannons/">Cannons</a></td><td class="has-text-align-center" data-align="center">17.86</td></tr><tr><td class="has-text-align-center" data-align="center">71st</td><td><a href="/teams/cambridgeshire-crossbows/">Crossbows</a></td><td class="has-text-align-center" data-align="center">19.67</td></tr><tr><td class="has-text-align-center" data-align="center">79th</td><td><a href="/teams/cambridgeshire-crusaders/">Crusaders</a></td><td class="has-text-align-center" data-align="center">20.27</td></tr><tr><td class="has-text-align-center" data-align="center">91st</td><td><a href="/teams/cambridgeshire-chargers/">Chargers</a></td><td class="has-text-align-center" data-align="center">21.10</td></tr>
             foreach ($results as $row){
                 echo '<tr>
-                <td class="has-text-align-center" data-align="center">'.getOrdinal($row->seed_postition).'</td>
+                <td class="has-text-align-center" data-align="center">'.getOrdinal($row->seed_position).'</td>
                 <td><a href="/teams/'.$row->slug.'/" data-type="URL">'.$row->team.'</a></td>
                 <td class="has-text-align-center" data-align="center">'.number_format($row->seed_time, 2).'</td></tr>';
             }
@@ -76,58 +76,6 @@ class cft_widget extends WP_Widget {
     
     // Class cft_widget ends here
 } 
-
-function getOrdinal($number=0){
-    // Handles special case three digit numbers ending
-	// with 11, 12 or 13 - ie, 111th, 112th, 113th, 211th, et al
-	If ($number > 99) {
-					$intEndNum = substr($number,-2);
-					If ($intEndNum >= 11 And $intEndNum <= 13) {
-									switch ($intEndNum){
-													Case (11 or 12 or 13):
-													Return "th";
-													break;
-									}
-					}
-	}
-	If ($number >= 21) {
-	// Handles 21st, 22nd, 23rd, et al
-					switch (substr($number,-1)) {
-									Case 0:
-									Return "th";
-									break;
-									Case 1:
-									Return "st";
-									break;
-									Case 2:
-									Return "nd";
-									break;
-									Case 3:
-									Return "rd";
-									break;
-									Case (4 || 5 || 6 || 7 || 8 || 9):
-									Return "th";
-									break;
-					}
-	} else {
-					// Handles 1st to 20th
-					switch ($number){
-									Case 1:
-									Return "st";
-									break;
-									Case 2:
-									Return "nd";
-									break;
-									Case 3:
-									Return "rd";
-									break;
-									Case (4 || 5 || 6 || 7 || 8 || 9 || 10 || 11 || 12 || 13 || 14 || 15 || 16 || 17 || 18 || 19 || 20):
-									Return "th";
-									break;
-					}
-	}
-} // end func am_GetOrdinal
-
  
  
 // Register and load the widget
